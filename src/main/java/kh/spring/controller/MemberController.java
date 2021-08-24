b60c4303b8e7a1d202ba6ea7aaa5e0341f04519b
yun <yun@naver.com>
2021-08-24 16:41:24 +0900

    *

--- src/main/java/kh/spring/controller/MemberController.java
+++ src/main/java/kh/spring/controller/MemberController.java
@@ -80,8 +80,9 @@ public class MemberController {
 			if(login != null) {
 				System.out.println(login.getCm_pw());
 				String hash_password = login.getCm_pw();
+				System.out.println(login.getCm_id());
 				if(BCrypt.checkpw(cm_pw, hash_password)) {
-				session.setAttribute("loginID", login);
+				session.setAttribute("loginID", login.getCm_id());
 				} else {
 				session.setAttribute("loginID", null);
 				}
